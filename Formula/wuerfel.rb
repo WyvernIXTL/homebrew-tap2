6193a92c2d2c1b240b51c97977236eab817520a5
Adam McKellar <dev@mckellar.eu>
2025-09-19 22:11:35 +0000

    wuerfel: add 0.1.11 bottle.

--- Formula/wuerfel.rb
+++ Formula/wuerfel.rb
@@ -5,6 +5,12 @@ class Wuerfel < Formula
   sha256 "f34292725e0bc7a4b7a06d0de81264097a4a2b918a5d620315258606b67499e3"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/wuerfel-0.1.11"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "cff897965d7c80394db56305374fd36779a93dda82d8b4908a3de15bb0274682"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ecaa332349954cc91725fb5210d293d5b4deb5ecf428b955a0e2e74cba2c50bb"
+  end
+
   depends_on "rust" => :build
   depends_on "expat"
 
