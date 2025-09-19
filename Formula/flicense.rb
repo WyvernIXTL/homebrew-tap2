dc3990de44332993770986f5f3e61038bbcc47d3
Adam McKellar <dev@mckellar.eu>
2025-09-19 22:41:15 +0000

    flicense: add 0.3.0 bottle.

--- Formula/flicense.rb
+++ Formula/flicense.rb
@@ -5,6 +5,13 @@ class Flicense < Formula
   sha256 "f03db1feaae5671bcdcdaa43000616e30792c73f9826def79c2e2bdac0f9e750"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/flicense-0.3.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "2578d5b4ca3ffb392ff5c66ecf2242cccf61f842e9c0c5e8db55c57a860462b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0164d0c34d13203498064b3cea9a81eec262d2a42a51002a5758c0d2e112c979"
+  end
+
   depends_on "rust" => :build
   depends_on "expat"
 
