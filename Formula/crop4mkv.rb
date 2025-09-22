19470df62a8c5f526ce0891287b75217a135c33c
Adam McKellar <dev@mckellar.eu>
2025-09-22 23:09:33 +0000

    crop4mkv: add 0.7.3 bottle.

--- Formula/crop4mkv.rb
+++ Formula/crop4mkv.rb
@@ -5,6 +5,13 @@ class Crop4mkv < Formula
   sha256 "5680c0ed65b2019732b5d9504ce4badb008daaabb47c466956aa691127e4983b"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/crop4mkv-0.7.3"
+    sha256                               arm64_tahoe:   "bf85bf76c3626f66d3792424d1df56fa38e10b1dd9c6b039a5232ca28fac62cc"
+    sha256                               arm64_sequoia: "a972094eaaee37d180debe8565c71a7d972922dfa0a99f2202951e7bb9c3acb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0822ffdf37fbd5e5d9befc56ed87ff0c1ad6105643ce3d005b737182728e2fe0"
+  end
+
   depends_on "pkgx" => :build
   depends_on "ffmpeg"
   depends_on "mkvtoolnix"
