cc8cf1c7054777ee5045a39746f7f83cbb6dd4d1
Adam McKellar <dev@mckellar.eu>
2025-09-22 23:11:06 +0200

    feat: add crop4mkv

---
+++ Formula/crop4mkv.rb
@@ -0,0 +1,26 @@
+class Crop4mkv < Formula
+  desc "Bun ts script that analyses crop margins of a video and sets the margins"
+  homepage "https://github.com/WyvernIXTL/crop4mkv"
+  url "https://github.com/WyvernIXTL/crop4mkv/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "5680c0ed65b2019732b5d9504ce4badb008daaabb47c466956aa691127e4983b"
+  license "MPL-2.0"
+
+  depends_on "go-task" => :build
+  depends_on "oven-sh/bun/bun" => :build
+  depends_on "powershell/tap/powershell-lts" => :build
+  depends_on "ffmpeg"
+  depends_on "mkvtoolnix"
+
+  def install
+    system "bun", "install", "--production"
+    system "bun", "build", "--compile", "--minify", "--sourcemap", "./src/index.ts", "--outfile", "crop4mkv"
+    bin.install "crop4mkv"
+  end
+
+  test do
+    system bin/"crop4mkv", "--help"
+    system bin/"crop4mkv", "-g", "."
+  end
+end
+
+
