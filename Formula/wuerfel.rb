3bd7e1491a0b2c60871b1404bba18a1eb057fa1d
Adam McKellar <dev@mckellar.eu>
2026-04-28 21:20:39 +0000

    wuerfel: update 0.1.13 bottle.

--- Formula/wuerfel.rb
+++ Formula/wuerfel.rb
@@ -6,11 +6,11 @@ class Wuerfel < Formula
   license "MPL-2.0"
 
   bottle do
-    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/wuerfel-0.1.12_1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cd9ea070dd167acd9c95355e702946f7bcef45f6a0e69b7e585bd9f2ac0855d9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07a1bdfe5083659e1673926b296fafc15905e704d6ab63e665043550fb3470d3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7b5708edf33fe0f8411198a46c4d4fb7cfd7a5e808d9da761a99f5c1facc8dc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "550faba32f30311f493a995a6033ddfa76b8d5fb2f79609f2ea1a6b4711cf963"
+    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/wuerfel-0.1.13"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38b637ede79795832add04a8ef35a04e3d7b85fa74317dca19a55b5a97d53764"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69b8d19d7479b9cbdea09b4340e854d738df176db3993295dddcbc3ec118f5b0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2584d0ba4503c8a1992cb4fbcad896c745f20a38c606003e3cf7d6e6e6b72fd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2eb187df07b3e101b9229482527cecc152b477cb353287efe02b5b663f540a18"
   end
 
   depends_on "expat" => :build
