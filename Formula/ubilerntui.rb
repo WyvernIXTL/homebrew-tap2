4c67f088960ebf1a1f355ffbbd8c456df4fdac61
Adam McKellar <dev@mckellar.eu>
2026-04-30 15:32:52 +0000

    ubilerntui: add 0.1.10_1 bottle.

--- Formula/ubilerntui.rb
+++ Formula/ubilerntui.rb
@@ -6,6 +6,13 @@ class Ubilerntui < Formula
   license "GPL-3.0-or-later"
   revision 1
 
+  bottle do
+    root_url "https://github.com/WyvernIXTL/homebrew-tap2/releases/download/ubilerntui-0.1.10_1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "302595d0a360f8faff31879fcddef0e58a097751ebc1f417a38dae85793901d5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "9ae83d44955b9a157d2c7b53266f8a279aa84f32453caeb6affc2b5f050422fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8e6186030bbef79d60f90c98f3f978112fa130701c6a324cb8b654b1c8e0da8b"
+  end
+
   depends_on "expat" => :build
   depends_on "rust" => :build
 
